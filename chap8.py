483e85e237da267f613570ce52e8d41ebadc74ec
ChunEunyu <82995817+ChunEunyu@users.noreply.github.com>
2022-01-10 20:33:45 +0900

    8장 푸는 중-12문제 남음

--- chap8.py
+++ chap8.py
@@ -1,8 +1,3 @@
-n = input("정수 n을 입력하시오: ")
-
-def Fibonacci(n):
-    for i in range(0, n):
-        print("%d, "%i)
-        result = i
-        result 
-        print("%d, "%result)
+class Calculator:
+    def sum():
+        

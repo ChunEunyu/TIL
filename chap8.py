8b465b48950b4dfd64ff589bdf9475bb9b6d698d
ChunEunyu <82995817+ChunEunyu@users.noreply.github.com>
2022-01-11 22:58:40 +0900

    8장 15번까지 풀이 완료

--- chap8.py
+++ chap8.py
@@ -1,14 +1,14 @@
-f = open('sample.txt', 'r')
-lines = f.readlines()    # 모든 라인을 읽음
-f.close()
+def chkDupNum(s):
+    result = []
+    for num in s: 
+        if num not in result:   # result 리스트에 없는 수
+            result.append(num)  # result 리스트에 추가
+        else:                   # result 리스트에 있는 수
+            return False        # False
+    return len(result) == 10    # result 요소 개수가 10개면 True
 
-total=0
-for line in lines:
-    score = int(line)
-    total += score
-
-avg = total/len(lines)
-
-f=open("result.txt", "w")
-f.write(str(avg))
-f.close()
+print(chkDupNum("0123456789"))      # True 리턴
+print(chkDupNum("01234"))           # False 리턴
+print(chkDupNum("01234567890"))     # False 리턴
+print(chkDupNum("6789012345"))      # True 리턴
+print(chkDupNum("012322456789"))    # False 리턴

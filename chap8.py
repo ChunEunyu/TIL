ac74aa1b4b4f6be79065e3b4e07699d3a6a2b33c
ChunEunyu <82995817+ChunEunyu@users.noreply.github.com>
2022-01-12 10:52:11 +0900

    8장 오답 완료

--- chap8.py
+++ chap8.py
@@ -1,14 +1,7 @@
-def chkDupNum(s):
-    result = []
-    for num in s: 
-        if num not in result:   # result 리스트에 없는 수
-            result.append(num)  # result 리스트에 추가
-        else:                   # result 리스트에 있는 수
-            return False        # False
-    return len(result) == 10    # result 요소 개수가 10개면 True
+import re
 
-print(chkDupNum("0123456789"))      # True 리턴
-print(chkDupNum("01234"))           # False 리턴
-print(chkDupNum("01234567890"))     # False 리턴
-print(chkDupNum("6789012345"))      # True 리턴
-print(chkDupNum("012322456789"))    # False 리턴
+pat = re.compile(".*[@].*[.](?=com$|net$).*$")
+
+print(pat.match("pahkey@gmail.com"))
+print(pat.match("kim@daum.net"))
+print(pat.match("lee@myhome.co.kr"))
